a9216af5122be5f2941c89754eae26d4ef4482b9
Bima <flowgreen173@gmail.com>
2025-04-16 13:19:59 +0700

    Minor Update

--- lib/modul_6/main.dart
+++ lib/modul_6/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:praktikum_2_modul/modul_6/bottomnav.dart';
+import 'package:praktikum_2_modul/modul_6/form.dart';
+import 'package:praktikum_2_modul/modul_6/tabcontroller.dart';
 
 void main() {
   runApp(
-    BotApp()
-    //GalleryPage()
+    // BotApp()
+    // TabBarDemo()
+    Forms()
     );
 }
